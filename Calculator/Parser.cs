4d5a87e99ff0e6c62ba408ab3cf9b9aab8b72897
ComedyStudios <39218621+MischaWillKekse@users.noreply.github.com>
2021-04-25 17:41:49 +0200

    renamed files

--- Calculator/Parser.cs
+++ Calculator/Parser.cs
@@ -6,8 +6,7 @@ using System.Diagnostics;
 namespace ConsoleApp1
 {
     public class Parser
-    { 
-        
+    {
         private string[] NumberCharArray = {"0","1","2","3","4","5","6","7","8","9","."}; 
         private string[] OpperatorNameArray = {"+","-","*","/","("};
         private List<string> NewExpression = new List<string>(); 
