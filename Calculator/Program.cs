298c8db5e79ee3d7f878191dadb16644c2d6f2e1
ComedyStudios <39218621+MischaWillKekse@users.noreply.github.com>
2021-05-04 09:15:44 +0200

    updated the gitignore

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -23,3 +23,22 @@ namespace ConsoleApp1
         }
     }
 }
+
+/*TODO (7*7-9)+60 
+(7* 7-   9)+60
+(7.7-4)/5
+(7,7-4)/5
+7.7-4-)5
+(7*7-9)+((60)
+4-3*-456+ (24)
++25-56
++25rte-56
++25rte-56
+
+a=5*5
+b=6
+a*6/b
+a*6/c
+*/
+
+//TODO read each line as new expression 
